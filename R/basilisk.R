68a8bc39ece6b6020c9871e7188baba889f781ea
Charlotte Soneson <charlottesoneson@gmail.com>
2022-12-12 14:15:23 +0100

    Fix versions of conda packages

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,10 +1,9 @@
 #' @importFrom basilisk BasiliskEnvironment
 dejunkerenv <- basilisk::BasiliskEnvironment(
     envname = "dejunker", pkgname = "deJUNKER",
-    packages = c("tensorflow-hub", "scipy==1.7.3",
-                 "requests", "Pillow", "h5py", "pydot",
-                 "pandas==1.3.5", "tensorflow==2.11.0",
-                 "keras==2.11.0"),
-    channels = c("bioconda", "conda-forge"),
-    pip = c("")
+    packages = c("tensorflow-hub==0.12.0", "scipy==1.7.3",
+                 "requests==2.28.1", "Pillow==9.2.0", "h5py==3.7.0",
+                 "pandas==1.3.5", "tensorflow==2.10.0",
+                 "keras==2.10.0", "pydot==1.4.2"),
+    channels = c("bioconda", "conda-forge")
 )
