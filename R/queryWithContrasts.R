039b6af9ab3d37dee49a638e760601dcbee3d66f
ppapasaikas <panp80@gmail.com>
2022-12-13 14:26:32 +0100

    Passing annotation vector to plotting function

--- R/queryWithContrasts.R
+++ R/queryWithContrasts.R
@@ -32,94 +32,95 @@ queryWithContrasts <- function(contrasts = NULL,
                                preserveInGlobalEnv = TRUE,
                                plotContrast = c("RESIDUAL", "INPUT", "DECODED", "NONE"),
                                detailTopn = 10) {
-    
-    stopifnot("`contrasts` should be a valid SummarizedExperiment" = is(contrasts, "SummarizedExperiment"))
-    valid.contrasts <- c("INPUT_CONTRASTS", "DECODED_CONTRASTS", "RESIDUAL_CONTRASTS")
-    present.contrasts <- intersect(valid.contrasts, names(assays(contrasts)))
-    stopifnot("The assays slot in the provided SummarizedExperiment does not contain valid contrast names " = length(present.contrasts) > 0)
-    message(paste ("provided contrast: ", present.contrasts, collapse = "\n"))
-    
-    plotContrast <-  match.arg(plotContrast)
-    
-    if (!exists ("target.contrasts")) {
-      message("Loading contrast database...")
-      target.contrasts <- readRDS("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_human_v212_uncompressed.rds")
-      ### Probably better to move to HDF5 based implementation as this will 
-      ### be both a significant speed-up in terms of loading
-      ### and much more lean on memory requirements. However this needs a 
-      ### reworked implementation using DelayedMatrices.
-      #   target.contrasts <- loadHDF5SummarizedExperiment(dir="/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_HDF5/",
-      #                                               prefix="human_v212")
-      if (preserveInGlobalEnv){
-        assign("target.contrasts", target.contrasts, envir = .GlobalEnv)
-      }
+  
+  stopifnot("`contrasts` should be a valid SummarizedExperiment" = is(contrasts, "SummarizedExperiment"))
+  valid.contrasts <- c("INPUT_CONTRASTS", "DECODED_CONTRASTS", "RESIDUAL_CONTRASTS")
+  present.contrasts <- intersect(valid.contrasts, names(assays(contrasts)))
+  stopifnot("The assays slot in the provided SummarizedExperiment does not contain valid contrast names " = length(present.contrasts) > 0)
+  message(paste ("provided contrast: ", present.contrasts, collapse = "\n"))
+  
+  plotContrast <-  match.arg(plotContrast)
+  
+  if (!exists ("target.contrasts")) {
+    message("Loading contrast database...")
+    target.contrasts <- readRDS("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_human_v212_uncompressed.rds")
+    ### Probably better to move to HDF5 based implementation as this will 
+    ### be both a significant speed-up in terms of loading
+    ### and much more lean on memory requirements. However this needs a 
+    ### reworked implementation using DelayedMatrices.
+    #   target.contrasts <- loadHDF5SummarizedExperiment(dir="/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_HDF5/",
+    #                                               prefix="human_v212")
+    if (preserveInGlobalEnv){
+      assign("target.contrasts", target.contrasts, envir = .GlobalEnv)
     }
-    
-    smpl.col <- if ( ncol(target.contrasts)==75 ) 1:75 else seq(1, 75000, 1000)   # A sample of col indices to quickly check data integrity
-    DBhash <- digest::digest(target.contrasts[,smpl.col], algo = "xxhash64")
-    stopifnot("The contrast DB contained in the `target.contrasts` object has not been correctly loaded.
+  }
+  
+  smpl.col <- if ( ncol(target.contrasts)==75 ) 1:75 else seq(1, 75000, 1000)   # A sample of col indices to quickly check data integrity
+  DBhash <- digest::digest(target.contrasts[,smpl.col], algo = "xxhash64")
+  stopifnot("The contrast DB contained in the `target.contrasts` object has not been correctly loaded.
 Please remove `target.contrasts` and try again." = DBhash == "99c9726494690892")
-    
-    stopifnot( "Incompatible rownames in the provided SummarizedExperiment.
+  
+  stopifnot( "Incompatible rownames in the provided SummarizedExperiment.
 Rownames should be the same as in the contrast database.
 You can make sure by generating your SE generated using `decomposeVar`"
-               = identical(rownames(contrasts), rownames(target.contrasts)))
-    contrasts <- assays(contrasts)[present.contrasts]
+             = identical(rownames(contrasts), rownames(target.contrasts)))
+  contrasts <- assays(contrasts)[present.contrasts]
+  
+  
+  if (use == "expressed.in.both") {
+    #Set a global expression threshold according to a quantile in the query data context
+    message("Thresholding genes...")
+    thr <- quantile(contrasts[["CONTEXT"]], exprThr)
+    set.to.NA <- assays(target.contrasts)[["CONTEXT"]] <= thr
     
-    
-    if (use == "expressed.in.both") {
-        #Set a global expression threshold according to a quantile in the query data context
-        message("Thresholding genes...")
-        thr <- quantile(contrasts[["CONTEXT"]], exprThr)
-        set.to.NA <- assays(target.contrasts)[["CONTEXT"]] <= thr
-        
-        pearson.rhos <- sapply(present.contrasts, function(x) {
-            message(paste0("Querying contrast database with ", x, "..."))
-            Thresholded.Contrast <- assays(target.contrasts)[[x]]
-            Thresholded.Contrast[ set.to.NA ] <- NA
-            stats::cor(contrasts[[x]], Thresholded.Contrast, 
-                use = "pairwise.complete.obs") 
-            }, simplify = FALSE, USE.NAMES = TRUE
-        )
-    } else if (use == "all.genes") {
-        pearson.rhos <- sapply(present.contrasts, function(x) {
-            message(paste0("Querying contrast database with ", x, "..."))
-            stats::cor(contrasts[[x]], assays(target.contrasts)[[x]])  
-            }, simplify = FALSE, USE.NAMES = TRUE
-        )
-    }
-    
-    ### Z-score calculation for each decomposed component query:
-    message("Compiling query statistics...")
-    zscores <- sapply(present.contrasts, function(x) {
-        t(scale(t(pearson.rhos[[x]])))
+    pearson.rhos <- sapply(present.contrasts, function(x) {
+      message(paste0("Querying contrast database with ", x, "..."))
+      Thresholded.Contrast <- assays(target.contrasts)[[x]]
+      Thresholded.Contrast[ set.to.NA ] <- NA
+      stats::cor(contrasts[[x]], Thresholded.Contrast, 
+                 use = "pairwise.complete.obs") 
     }, simplify = FALSE, USE.NAMES = TRUE
     )
-    
-    TopHits <- sapply(present.contrasts, function(contr){
-        apply(zscores[[contr]], 1, function(x) { 
-            N <- names(sort(x, decreasing = TRUE)[seq_len(detailTopn)])
-        colData(target.contrasts)[N, c(3, 12, 22, 24, 29, 31, 33)]
-        })
+  } else if (use == "all.genes") {
+    pearson.rhos <- sapply(present.contrasts, function(x) {
+      message(paste0("Querying contrast database with ", x, "..."))
+      stats::cor(contrasts[[x]], assays(target.contrasts)[[x]])  
     }, simplify = FALSE, USE.NAMES = TRUE
     )
-    
-    if (plot.contrast != "NONE" && 
-        paste0(plot.contrast, "_CONTRASTS") %in% present.contrasts) {
-        message("Generating plots...")
-        plot.data <- zscores[[paste0(plot.contrast, "_CONTRASTS")]]
-        PLOTLIST <- sapply(seq_len(nrow(plot.data)), function(i) {
-            plotQueryResults(plot.data[i, ])
-        }, simplify = FALSE)
-        suppressWarnings({
-            print(
-                cowplot::plot_grid(plotlist = PLOTLIST,
-                                   labels = rownames(plot.data))
-            )
-        })
-    }
-    
-    message("Done!")
-    return(list(pearson.rhos = pearson.rhos, zscores = zscores,
-                TopHits = TopHits))
+  }
+  
+  ### Z-score calculation for each decomposed component query:
+  message("Compiling query statistics...")
+  zscores <- sapply(present.contrasts, function(x) {
+    t(scale(t(pearson.rhos[[x]])))
+  }, simplify = FALSE, USE.NAMES = TRUE
+  )
+  
+  TopHits <- sapply(present.contrasts, function(contr){
+    apply(zscores[[contr]], 1, function(x) { 
+      N <- names(sort(x, decreasing = TRUE)[seq_len(detailTopn)])
+      colData(target.contrasts)[N, c(3, 12, 22, 24, 29, 31, 33)]
+    })
+  }, simplify = FALSE, USE.NAMES = TRUE
+  )
+  
+  if (plot.contrast != "NONE" && 
+      paste0(plot.contrast, "_CONTRASTS") %in% present.contrasts) {
+    message("Generating plots...")
+    plot.data <- zscores[[paste0(plot.contrast, "_CONTRASTS")]]
+    PLOTLIST <- sapply(seq_len(nrow(plot.data)), function(i) {
+      plotQueryResults(plot.data[i, ],annot=target.contrasts$series_id,topn=detailTopn)
+      
+    }, simplify = FALSE)
+    suppressWarnings({
+      print(
+        cowplot::plot_grid(plotlist = PLOTLIST,
+                           labels = rownames(plot.data))
+      )
+    })
+  }
+  
+  message("Done!")
+  return(list(pearson.rhos = pearson.rhos, zscores = zscores,
+              TopHits = TopHits))
 }
