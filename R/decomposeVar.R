2a6caca17df76a7e672d31f8b0eeb3bb0f7e6cc1
ppapasaikas <panp80@gmail.com>
2022-12-14 17:00:23 +0100

    Substitute code for Delta model definition and weight loading with one-step model loading.

--- R/decomposeVar.R
+++ R/decomposeVar.R
@@ -167,7 +167,7 @@ Symbol, Ensembl or Entrez gene identifiers  for the specified organism as rownam
         message("Encoding and decoding contrasts...")
     }
     res <- basiliskRun(env = dejunkerenv, fun = .predict_encoderd, 
-                       delta_input = D, context = LATC, ngenes = ngenes)
+                       delta_input = D, context = LATC)
     LATD <- res$LATD
     DEC <- res$DEC
     
@@ -219,112 +219,12 @@ Symbol, Ensembl or Entrez gene identifiers  for the specified organism as rownam
 #'     k_square k_sqrt loss_mean_squared_error
 #' @importFrom tensorflow tf
 #' 
-.predict_encoderd <- function(delta_input, context, ngenes) {
-    K <- keras::backend()
-    if (tensorflow::tf$executing_eagerly())
-        tensorflow::tf$compat$v1$disable_eager_execution()
+.predict_encoderd <- function(delta_input, context) {
+    ### Load contrast encoder and generator models:
+    encoderD <- keras::load_model_hdf5  ("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Trained_models/Model_encoderD_deJUNKER_ARCHS4_v212_ftune_512_64_human.hdf5")
+    generatorD <- keras::load_model_hdf5("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Trained_models/Model_generatorD_deJUNKER_ARCHS4_v212_ftune_512_64_human.hdf5")
     
-    # Parameters --------------------------------------------------------------
-    latentD <- 512L # Latent dimension for Delta encoder
-    latentC <- 64L # Latent dimension for Context encoder
-    drop_rate <- 0.2 # Dropout rate
-    gene_dim <- ngenes # Number of features (genes) in the dataset
-    epsilon_std <- 0.5 # Standard deviation of the prior latent distribution (vanilla = 1)
-    var_prior <- epsilon_std**2
-    log_var_prior <- log(var_prior)
-    kl_weight <- 0.20 # Weight for the Delta Kulllback-Leibler divergence loss (vanilla = 1)
-    
-    ### Define encoder input layers:
-    xD <- keras::layer_input(shape = c(gene_dim), name = "delta_input")
-    cont_input <- keras::layer_input(shape = c(latentC), name = 'CONTEXT')
-    all_inputs <- keras::layer_concatenate(list(xD, cont_input))
-    
-    #### Delta encoder definition:
-    hD <- keras::layer_dense(all_inputs, 4 * latentD, activation = "elu")
-    hD <- keras::layer_dropout(hD, rate = drop_rate)
-    hD <- keras::layer_dense(hD, 2 * latentD, activation = "elu")
-    hD <- keras::layer_dropout(hD, rate = drop_rate)
-    z_meanD <- keras::layer_dense(hD, latentD)
-    z_log_varD <- keras::layer_dense(hD, latentD)
-    
-    # Define delta encoder
-    encoderD <- keras::keras_model(inputs = c(xD, cont_input), outputs = z_meanD)
-    
-    #### Sampling from the Delta latent space:
-    samplingD <- function(arg) {
-        z_meanD <- arg[, seq_len(latentD)]
-        z_log_varD <- arg[, (latentD + 1):(2 * latentD)]
-        epsilonD <- K$random_normal(
-            shape = c(K$shape(z_meanD)[[1]]),
-            mean = 0.,
-            stddev = epsilon_std
-        )
-        z_meanD + K$exp(z_log_varD/2) * epsilonD
-    }
-    
-    # Lambda layer for variational sampling:
-    zD <- keras::layer_concatenate(list(z_meanD, z_log_varD)) %>%
-        keras::layer_lambda(samplingD)
-    
-    # Merge delta latent space with context latent space:
-    z_concat <- keras::layer_concatenate(list(zD, cont_input))
-    
-    # Define layers for the Delta decoder (no batch-norm. Seems to increase overfitting):
-    decoder_h1 <- keras::layer_dense(units = 2*latentD, activation = "elu")
-    decoder_h2 <- keras::layer_dropout(rate = drop_rate)
-    decoder_h3 <- keras::layer_dense(units = 4*latentD, activation = "elu")
-    decoder_h4 <- keras::layer_dropout(rate = drop_rate)
-    
-    decoder_out <- keras::layer_dense(units = ngenes, activation = "linear")
-    
-    h_p <- decoder_h1(z_concat)
-    h_p <- decoder_h2(h_p)
-    h_p <- decoder_h3(h_p)
-    h_p <- decoder_h4(h_p)
-    outputs <- decoder_out(h_p)
-    
-    # Define full cvae (Input: lcpm, deltas  Target Outputs: Decoded deltas)
-    cvae <- keras::keras_model(inputs = c(xD, cont_input), outputs)
-    
-    # Reuse decoder layers to define the generator (concatenated latent space to gene output) separately
-    d_in <- layer_input(shape = latentC + latentD)
-    d_h <- decoder_h1(d_in)
-    d_h <- decoder_h2(d_h)
-    d_h <- decoder_h3(d_h)
-    d_h <- decoder_h4(d_h)
-    d_out <- decoder_out(d_h)
-    generatorD <- keras::keras_model(d_in, d_out)
-    
-    cvae_loss <- function(x, x_decoded_mean) {
-        reconstruction_loss <- keras::loss_mean_squared_error(x, x_decoded_mean)
-        kl_loss <- -kl_weight * 0.5 * 
-            K$mean(1 + z_log_varD - log_var_prior - K$square(z_meanD) / var_prior - 
-                       K$exp(z_log_varD) / var_prior, axis = -1L)  # Delta KL loss
-        reconstruction_loss + kl_loss
-    }
-    
-    #### Custom correlation function to keep track of.
-    #### All mathematical operation NEED to be performed using the Keras backend (e.g K$mean):
-    cor_metric <- function(y_true, y_pred) {
-        y_true_dev <- y_true - keras::k_mean(y_true)
-        y_pred_dev <- y_pred - keras::k_mean(y_pred)
-        r_num <- keras::k_sum(y_true_dev * y_pred_dev)
-        r_den <- keras::k_sqrt(k_sum(keras::k_square(y_true_dev)) *
-                                   keras::k_sum(keras::k_square(y_pred_dev)))
-        r_num / r_den
-    }
-    
-    cvae %>% keras::compile(
-        loss = cvae_loss,
-        optimizer = "adam",
-        metrics = keras::custom_metric("cor", cor_metric)
-    )
-    
-    ### Load model weights:
-    # cvae %>% keras::load_model_weights_hdf5("/Users/charlottesoneson/Documents/Rpackages/deJUNKER/data/cvae_deJUNKER_ARCHS4_v212_ftune_512_64_v1bb.hdf5")
-    cvae %>%
-        keras::load_model_weights_hdf5("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Trained_models/cvae_deJUNKER_ARCHS4_v212_ftune_512_64_v1bb.hdf5")
-    
+    ### Encode and decode deltas:
     LATD <- predict(encoderD, list(delta_input = delta_input, CONTEXT = context))
     DEC <- t(predict(generatorD, cbind(LATD, context)))
     
