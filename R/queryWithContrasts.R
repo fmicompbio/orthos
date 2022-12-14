acbac1a29dfd56df9b420a1043c259630283ee7e
Charlotte Soneson <charlottesoneson@gmail.com>
2022-12-14 10:47:36 +0100

    Some formatting and dependencies

--- R/queryWithContrasts.R
+++ R/queryWithContrasts.R
@@ -22,54 +22,71 @@
 #'     accessing its metadata
 #' @param detailTopn specifies the number of top hits for which metadata will 
 #'     be returned in the TopHits slot of the results.
+#' @param verbose Logical scalar indicating whether to print messages along 
+#'     the way.
 #'
 #' @return A list of PearsonRhos, Zscores against the datbase as well as 
 #'     detailed Metadata for the detailTopn hits.
+#' 
+#' @importFrom digest digest
 #' 
 queryWithContrasts <- function(contrasts = NULL, 
                                use = c("expressed.in.both", "all.genes"),
                                exprThr = 0.25, organism = "Human", 
                                preserveInGlobalEnv = TRUE,
                                plotContrast = c("RESIDUAL", "INPUT", "DECODED", "NONE"),
-                               detailTopn = 10) {
+                               detailTopn = 10, verbose = TRUE) {
     
+    ## -------------------------------------------------------------------------
+    ## Check inputs
+    ## -------------------------------------------------------------------------
     stopifnot("`contrasts` should be a valid SummarizedExperiment" = 
                   is(contrasts, "SummarizedExperiment"))
     valid.contrasts <- c("INPUT_CONTRASTS", "DECODED_CONTRASTS", "RESIDUAL_CONTRASTS")
     present.contrasts <- intersect(valid.contrasts, names(assays(contrasts)))
-    stopifnot("The assays slot in the provided SummarizedExperiment does not contain valid contrast names " = length(present.contrasts) > 0)
+    stopifnot("The assays slot in the provided SummarizedExperiment does not contain valid contrast names " = 
+                  length(present.contrasts) > 0)
     message(paste ("provided contrast: ", present.contrasts, collapse = "\n"))
     
-    plotContrast <-  match.arg(plotContrast)
+    plotContrast <- match.arg(plotContrast)
+    use <- match.arg(use)
     
-    if (!exists ("target.contrasts")) {
+    ## -------------------------------------------------------------------------
+    ## Load contrast database
+    ## -------------------------------------------------------------------------
+    if (verbose) {
         message("Loading contrast database...")
-        target.contrasts <- readRDS("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_human_v212_uncompressed.rds")
-        ### Probably better to move to HDF5 based implementation as this will 
-        ### be both a significant speed-up in terms of loading
-        ### and much more lean on memory requirements. However this needs a 
-        ### reworked implementation using DelayedMatrices.
-        #   target.contrasts <- loadHDF5SummarizedExperiment(dir="/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_HDF5/",
-        #                                               prefix="human_v212")
-        if (preserveInGlobalEnv){
-            assign("target.contrasts", target.contrasts, envir = .GlobalEnv)
-        }
+    }
+    target.contrasts <- readRDS("/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_human_v212_uncompressed.rds")
+    # target.contrasts <- readRDS("/Users/charlottesoneson/Documents/Rpackages/deJUNKER/data/DECOMPOSED_CONTRASTS_human_v212_uncompressed_smpl.rds")
+    ### Probably better to move to HDF5 based implementation as this will 
+    ### be both a significant speed-up in terms of loading
+    ### and much more lean on memory requirements. However this needs a 
+    ### reworked implementation using DelayedMatrices.
+    #   target.contrasts <- loadHDF5SummarizedExperiment(dir="/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_HDF5/",
+    #                                               prefix="human_v212")
+    if (preserveInGlobalEnv) {
+        assign("target.contrasts", target.contrasts, envir = .GlobalEnv)
     }
     
-    smpl.col <- if ( ncol(target.contrasts)==75 ) 1:75 else seq(1, 75000, 1000)   # A sample of col indices to quickly check data integrity
-    DBhash <- digest::digest(target.contrasts[,smpl.col], algo = "xxhash64")
+    # A sample of col indices to quickly check data integrity
+    smpl.col <- if (ncol(target.contrasts) == 75) 1:75 else seq(1, 75000, 1000)
+    DBhash <- digest::digest(target.contrasts[, smpl.col], algo = "xxhash64")
     stopifnot("The contrast DB contained in the `target.contrasts` object has not been correctly loaded.
-Please remove `target.contrasts` and try again." = DBhash == "99c9726494690892")
+Please remove `target.contrasts` and try again." = 
+                  DBhash == "99c9726494690892")
     
     stopifnot( "Incompatible rownames in the provided SummarizedExperiment.
 Rownames should be the same as in the contrast database.
-You can make sure by generating your SE generated using `decomposeVar`"
-               = identical(rownames(contrasts), rownames(target.contrasts)))
+You can make sure by generating your SE generated using `decomposeVar`" = 
+                   identical(rownames(contrasts), rownames(target.contrasts)))
     contrasts <- assays(contrasts)[present.contrasts]
     
-    
+    ## -------------------------------------------------------------------------
+    ## Calculate correlations
+    ## -------------------------------------------------------------------------
     if (use == "expressed.in.both") {
-        #Set a global expression threshold according to a quantile in the query data context
+        # Set a global expression threshold according to a quantile in the query data context
         message("Thresholding genes...")
         thr <- quantile(contrasts[["CONTEXT"]], exprThr)
         set.to.NA <- assays(target.contrasts)[["CONTEXT"]] <= thr
@@ -90,14 +107,21 @@ You can make sure by generating your SE generated using `decomposeVar`"
         )
     }
     
-    ### Z-score calculation for each decomposed component query:
-    message("Compiling query statistics...")
+    ## -------------------------------------------------------------------------
+    ## Calculate z-scores for each decomposed component query
+    ## -------------------------------------------------------------------------
+    if (verbose) {
+        message("Compiling query statistics...")
+    }
     zscores <- sapply(present.contrasts, function(x) {
         t(scale(t(pearson.rhos[[x]])))
     }, simplify = FALSE, USE.NAMES = TRUE
     )
     
-    TopHits <- sapply(present.contrasts, function(contr){
+    ## -------------------------------------------------------------------------
+    ## Get top hits
+    ## -------------------------------------------------------------------------
+    TopHits <- sapply(present.contrasts, function(contr) {
         apply(zscores[[contr]], 1, function(x) { 
             N <- names(sort(x, decreasing = TRUE)[seq_len(detailTopn)])
             colData(target.contrasts)[N, c(3, 12, 22, 24, 29, 31, 33)]
@@ -105,13 +129,16 @@ You can make sure by generating your SE generated using `decomposeVar`"
     }, simplify = FALSE, USE.NAMES = TRUE
     )
     
-    if (plot.contrast != "NONE" && 
-        paste0(plot.contrast, "_CONTRASTS") %in% present.contrasts) {
+    ## -------------------------------------------------------------------------
+    ## Plot
+    ## -------------------------------------------------------------------------
+    if (plotContrast != "NONE" && 
+        paste0(plotContrast, "_CONTRASTS") %in% present.contrasts) {
         message("Generating plots...")
-        plot.data <- zscores[[paste0(plot.contrast, "_CONTRASTS")]]
+        plot.data <- zscores[[paste0(plotContrast, "_CONTRASTS")]]
         PLOTLIST <- sapply(seq_len(nrow(plot.data)), function(i) {
-            plotQueryResults(plot.data[i, ],annot=target.contrasts$series_id,topn=detailTopn)
-            
+            plotQueryResults(plot.data[i, ], annot = target.contrasts$series_id,
+                             topn = detailTopn)
         }, simplify = FALSE)
         suppressWarnings({
             print(
@@ -121,7 +148,10 @@ You can make sure by generating your SE generated using `decomposeVar`"
         })
     }
     
-    message("Done!")
+    if (verbose) {
+        message("Done!")
+    }
+    
     return(list(pearson.rhos = pearson.rhos, zscores = zscores,
                 TopHits = TopHits))
 }
