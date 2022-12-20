919b8cfd99f92f020f5167c61fa2ca015547411f
ppapasaikas <panp80@gmail.com>
2022-12-20 14:58:35 +0100

    Implementation using DelayedArrays.

--- R/queryWithContrasts_DA.R
+++ R/queryWithContrasts_DA.R
@@ -30,8 +30,9 @@
 #' 
 #' @importFrom digest digest
 #' @importFrom SummarizedExperiment assays
+#' @importFrom DelayedMatrixStats rowSds rowMeans2
 #' 
-queryWithContrasts <- function(contrasts = NULL, 
+queryWithContrasts_DA <- function(contrasts = NULL, 
                                use = c("expressed.in.both", "all.genes"),
                                exprThr = 0.25, organism = c("Human","Mouse"), 
                                preserveInGlobalEnv = TRUE,
@@ -62,12 +63,14 @@ queryWithContrasts <- function(contrasts = NULL,
         }
         
         ### reworked implementation using DelayedMatrices.
-        target.contrasts <- loadHDF5SummarizedExperiment(dir="/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_HDF5/",prefix="human_v212")
+        #target.contrasts <- loadHDF5SummarizedExperiment(dir="/tungstenfs/groups/gbioinfo/papapana/DEEP_LEARNING/Autoencoders/ARCHS4/Rdata/DECOMPOSED_CONTRASTS_HDF5/",prefix="human_v212")
+        target.contrasts <- readRDS(system.file("data", paste0("DECOMPOSED_CONTRASTS_ARCHS4_v212_",organism,"_smpl.rds"), package = "deJUNKER"))
+        
         if (preserveInGlobalEnv) {
             assign("target.contrasts", target.contrasts, envir = .GlobalEnv)
         }
     }
-
+    
     stopifnot( "Incompatible rownames in the provided SummarizedExperiment.
 Rownames should be the same as in the contrast database.
 You can make sure by generating your SE generated using `decomposeVar`" = 
@@ -77,28 +80,35 @@ You can make sure by generating your SE generated using `decomposeVar`" =
     ## -------------------------------------------------------------------------
     ## Calculate correlations
     ## -------------------------------------------------------------------------
-    if (use == "expressed.in.both") {
-        # Set a global expression threshold according to a quantile in the query data context
-        message("Thresholding genes...")
-        thr <- quantile(contrasts[["CONTEXT"]], exprThr)
-        set.to.NA <- SummarizedExperiment::assays(target.contrasts)[["CONTEXT"]] <= thr
-        
-        pearson.rhos <- sapply(present.contrasts, function(x) {
-            message(paste0("Querying contrast database with ", x, "..."))
-            Thresholded.Contrast <- SummarizedExperiment::assays(target.contrasts)[[x]]
-            Thresholded.Contrast[ set.to.NA ] <- NA
-            stats::cor(contrasts[[x]], Thresholded.Contrast, 
-                       use = "pairwise.complete.obs") 
-        }, simplify = FALSE, USE.NAMES = TRUE
-        )
-    } else if (use == "all.genes") {
-        pearson.rhos <- sapply(present.contrasts, function(x) {
-            message(paste0("Querying contrast database with ", x, "..."))
-            stats::cor(contrasts[[x]], SummarizedExperiment::assays(target.contrasts)[[x]])  
-        }, simplify = FALSE, USE.NAMES = TRUE
-        )
+    pearson.rhos <- list()
+    ## Calculate correlation
+    for (ctr in present.contrasts){
+        query <- contrasts[[ctr]]
+        targt <- SummarizedExperiment::assays(target.contrasts)[[ctr]]
+        pearson.rhos[[ctr]] <- do.call(cbind, mclapply(seq_len(ncol(query)), function(i) {
+            idx <- which(!is.na(query[, i]))
+            targtsub <- targt[idx, ]
+            querysub <- query[idx, i, drop = FALSE]
+            n_not_na <- t(!is.na(targtsub)) %*% matrix(1, nrow = length(idx), 
+                                                       ncol = 1)
+            query_sum_squared <- t(!is.na(targtsub)) %*% querysub ^ 2
+            query_sum <- t(!is.na(targtsub)) %*% querysub
+            
+            targtsub <- scale(targtsub, center = TRUE, scale = TRUE)
+            
+            ## Set NA values to 0
+            targtsub[is.na(targtsub)] <- 0
+            
+            ## Calculate correlation
+            ((t(targtsub) %*% querysub) / (n_not_na - 1)) / 
+                (sqrt(n_not_na * query_sum_squared - (query_sum) ^ 2) / 
+                     sqrt(n_not_na * (n_not_na - 1)))
+        }, mc.cores = 2L))
     }
     
+    
+    
+  
     ## -------------------------------------------------------------------------
     ## Calculate z-scores for each decomposed component query
     ## -------------------------------------------------------------------------
