d27d7ca6af24700fe1789098379cae62d40408b8
mbstadler <stadler.michael@gmail.com>
2022-12-23 11:56:51 +0100

    fix typo in unit test for .M2Mcor

--- tests/testthat/tests-helpers.R
+++ tests/testthat/tests-helpers.R
@@ -1,11 +1,11 @@
 ## ------------------------------------------------------------------------- ##
 ## Checks, M2Mcor
 ## ------------------------------------------------------------------------- ##
-test_that("M2Mcor works", {
+test_that(".M2Mcor works", {
     set.seed(1)
     M1 <- matrix(stats::rnorm(30), nrow = 10)
     M2 <- matrix(stats::rnorm(30), nrow = 10)
-    expect_equal(M2Mcor(M1, M2),
+    expect_equal(.M2Mcor(M1, M2),
                  c(stats::cor(M1[, 1], M2[, 1]),
                    stats::cor(M1[, 2], M2[, 2]),
                    stats::cor(M1[, 3], M2[, 3])))
