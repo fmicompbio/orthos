e8b5046c7078cecafc8977304e90ff29c49db7bf
mbstadler <stadler.michael@gmail.com>
2023-02-11 18:38:28 +0100

    use expect_s4_class instead of all(inherits(...))

--- tests/testthat/test-queryWithContrasts.R
+++ tests/testthat/test-queryWithContrasts.R
@@ -10,6 +10,8 @@ test_that(".loadContrastDatabase works", {
     expect_s4_class(seHuman, "SummarizedExperiment")
     expect_s4_class(seMouse, "SummarizedExperiment")
     
+    assayNms <- c("INPUT_CONTRASTS", "DECODED_CONTRASTS",
+                  "RESIDUAL_CONTRASTS", "CONTEXT")
     expect_identical(SummarizedExperiment::assayNames(seHuman),
                      c("INPUT_CONTRASTS", "DECODED_CONTRASTS",
                        "RESIDUAL_CONTRASTS", "CONTEXT"))
@@ -20,8 +22,10 @@ test_that(".loadContrastDatabase works", {
     expect_identical(dim(seHuman), c(20411L, 77016L))
     expect_identical(dim(seMouse), c(20339L, 59552L))
 
-    expect_true(all(unlist(lapply(SummarizedExperiment::assays(seHuman),
-                                  function(x) inherits(x, "DelayedArray")))))
-    expect_true(all(unlist(lapply(SummarizedExperiment::assays(seMouse),
-                                  function(x) inherits(x, "DelayedArray")))))
+    for (assayNm1 in assayNms) {
+        expect_s4_class(SummarizedExperiment::assay(seHuman, assayNm1),
+                        "DelayedArray")
+        expect_s4_class(SummarizedExperiment::assay(seMouse, assayNm1),
+                        "DelayedArray")
+    }
 })
