f6ecc7e3dc2a0fe18977e88716b0edef09329b69
Charlotte Soneson <charlottesoneson@gmail.com>
2025-04-09 09:13:37 +0200

    Avoid warnings about non-existing variable

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,19 @@
 # variable to avoid errors related to pybind11 and GIL starting with 
 # reticulate 1.42.0
 
+.orthos_pkg_env <- new.env(parent = emptyenv())
+
 .onLoad <- function(libname, pkgname) {
-    ORTHOS_RETICULATE_ENABLE_PYTHON_FINALIZER <- Sys.getenv("RETICULATE_ENABLE_PYTHON_FINALIZER")
+    .orthos_pkg_env$ORTHOS_RETICULATE_ENABLE_PYTHON_FINALIZER <- 
+        Sys.getenv("RETICULATE_ENABLE_PYTHON_FINALIZER", unset = NA)
     Sys.setenv("RETICULATE_ENABLE_PYTHON_FINALIZER" = "yes")
 }
 
-.onUnload <- function(libname, pkgname) {
-    if (exists(ORTHOS_RETICULATE_ENABLE_PYTHON_FINALIZER)) {
-        Sys.setenv("RETICULATE_ENABLE_PYTHON_FINALIZER" = ORTHOS_RETICULATE_ENABLE_PYTHON_FINALIZER)
+.onUnload <- function(libpath) {
+    if (!is.na(.orthos_pkg_env$ORTHOS_RETICULATE_ENABLE_PYTHON_FINALIZER)) {
+        Sys.setenv("RETICULATE_ENABLE_PYTHON_FINALIZER" = 
+                       .orthos_pkg_env$ORTHOS_RETICULATE_ENABLE_PYTHON_FINALIZER)
+    } else {
+        Sys.unsetenv("RETICULATE_ENABLE_PYTHON_FINALIZER")
     }
 }
