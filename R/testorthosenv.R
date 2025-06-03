d58d0ebf67e762402f8342a05a6a5e6dfd01e9db
ppapasaikas <panp80@gmail.com>
2025-06-03 15:46:55 +0200

    Add diagnostics for tf: physical devices ld library path tf build info

--- R/testorthosenv.R
+++ R/testorthosenv.R
@@ -16,9 +16,37 @@ testOrthosEnv <- function() {
     cl <- basiliskStart(orthosenv,
                         testload = "tensorflow")
     keras_tf_version <- basiliskRun(cl, function() {
-        list(keras_available = keras::is_keras_available("2.10.0"),
-             tf_version = tensorflow::tf$version$VERSION)
+        
+        
+        # Collect diagnostics
+        tf_version <- tensorflow::tf$version$VERSION
+        build_info <- tensorflow::tf$sysconfig$get_build_info()
+        devices <- tensorflow::tf$config$list_physical_devices()
+        keras_ok <- keras::is_keras_available("2.10.0")
+        ld_path <- Sys.getenv("LD_LIBRARY_PATH")
+        
+        # Print diagnostic info
+        cat("=== TensorFlow/Keras Diagnostic ===\n")
+        cat("TF Version: ", tf_version, "\n")
+        cat("LD_LIBRARY_PATH:\n", ld_path, "\n")
+        cat("Build Info:\n")
+        print(build_info)
+        cat("Physical Devices:\n")
+        print(devices)
+        cat("Keras Available: ", keras_ok, "\n")
+        cat("===================================\n")
+        
+        list(
+            keras_available = keras_ok,
+            tf_version = tf_version,
+            cuda_version = build_info$cuda_version,
+            cudnn_version = build_info$cudnn_version,
+            devices = unlist(lapply(devices, function(x) as.character(x$name))),
+            ld_library_path = ld_path
+        )
     })
     basiliskStop(cl)
     keras_tf_version
+        
+
 }
